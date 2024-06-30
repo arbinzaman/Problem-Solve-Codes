5826ff4c01b8c442b206e0eafd0be360d5ead3ad
arbinzaman <arbinzmn@gmail.com>
2024-07-01 02:51:44 +0600

    Chore: Solved Beecrowd problem [Distance]

---
+++ ProblemSolvedCodes/Becrowd/Distance/Distance.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, distance;
+    cin >> x;
+    distance = x * 2;
+    cout << distance <<" minutos" << endl;
+    return 0;
+}
