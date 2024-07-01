2fb2754152baf0c210aa187560d86bde852f8d77
arbinzaman <arbinzmn@gmail.com>
2024-07-01 23:26:35 +0600

    Solved Beecrowd Problem [1018 - Banknotes]

--- ProblemSolvedCodes/Becrowd/Banknotes/Banknotes.cpp
+++ ProblemSolvedCodes/Becrowd/Banknotes/Banknotes.cpp
@@ -1,10 +1,23 @@
-   #include<bits/stdc++.h>                                                                                                                                          
-    using namespace std;
-    int main(){
-        int a,b,c,MaiorAB,s,MAX;
-        cin>>a>>b>>c;
-        MaiorAB=(a+b+abs(a-b))/2;
-        MAX=(MaiorAB+c+abs(MaiorAB-c))/2;
-        cout<< MAX<<" eh o maior"<<endl;
-        return 0;
-    }
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int amount, aux;
+    cin >> amount;
+    cout<<amount<<endl;
+    cout << amount / 100 << " nota(s) de R$ 100,00" << endl;
+    aux = (amount % 100);
+    cout << aux / 50 << " nota(s) de R$ 50,00" << endl;
+    aux = (aux % 50);
+    cout << aux / 20 << " nota(s) de R$ 20,00" << endl;
+    aux = (aux % 20);
+    cout << aux / 10 << " nota(s) de R$ 10,00" << endl;
+    aux = (aux % 10);
+    cout << aux / 5 << " nota(s) de R$ 5,00" << endl;
+    aux = (aux % 5);
+    cout << aux / 2 << " nota(s) de R$ 2,00" << endl;
+    aux = (aux % 2);
+    cout << aux / 1 << " nota(s) de R$ 1,00" << endl;
+
+    return 0;
+}
