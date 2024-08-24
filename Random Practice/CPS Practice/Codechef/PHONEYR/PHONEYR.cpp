2705fb903e79ecaee417616efc677aa8fc2d55db
arbinzaman <arbinzmn@gmail.com>
2024-08-25 03:26:24 +0600

    chore: Solved Codechef problem

---
+++ Random Practice/CPS Practice/Codechef/PHONEYR/PHONEYR.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    // cout << "k" << ( x / 100 ) << endl;
+    // cout << "k" << (x % 100) << endl;
+    // cout << "k" << x - x / 10 * 10 << endl;
+    int y = x % 100;
+    cout << "k" ;
+    cout<< y / 10;
+    cout << y % 10 ;
+    cout<<endl;
+}
