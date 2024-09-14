d294a85a7cee6e0f26d4c95a1638a38dccb5b0fb
arbinzaman <arbinzmn@gmail.com>
2024-09-14 23:09:16 +0600

    chore: Added Shit operator notes CPS Class 13

--- Random Practice/CPS Practice/Random/Classes/13/Shift/shift.cpp
+++ Random Practice/CPS Practice/Random/Classes/13/Shift/shift.cpp
@@ -4,8 +4,14 @@ int main()
 {
     int a,b,c;
     cin >> a;
-    b = a << 3;/*left shit */
-    c= b >> 3;/*right shit*/
+    b = a << 3;  /*left shit */
+    c= b >> 3;  /*right shit*/
     cout << b<< " " << c;
+
+    
+    /** 
+    *!Shift is a bite wise operator , shift works in binary "2"
+    *!Bitwise operators works binary number system it shifts binary number
+    */
 
 }
