b32f9fdc681d40d90506662ed5d497db4b0c3f0f
arbinzaman <arbinzmn@gmail.com>
2024-09-14 23:29:52 +0600

    chore: Added Shifting operators code with notes

--- Random Practice/CPS Practice/Random/Classes/13/Shift/shift.cpp
+++ Random Practice/CPS Practice/Random/Classes/13/Shift/shift.cpp
@@ -8,10 +8,13 @@ int main()
     c= b >> 3;  /*right shit*/
     cout << b<< " " << c;
 
-    
+
     /** 
     *!Shift is a bite wise operator , shift works in binary "2"
     *!Bitwise operators works binary number system it shifts binary number
+    *!In Right Shift numbers reduced in right
+    *!If we want to know first digit of the odd number in binary we can use bitwise & with 1
+
     */
 
 }
