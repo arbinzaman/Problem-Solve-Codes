5fefe15d4048562b3b5394a790ed7e23a6ee9186
arbinzaman <arbinzmn@gmail.com>
2024-09-15 01:49:18 +0600

    chore: Added Advance Shifting practice code

---
+++ Random Practice/CPS Practice/Random/Classes/13/ShiftingAdvance/ShiftingAdvance.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, postion;
+    cin>>n>>postion;
+    int result  =(n>>postion)&1;
+    cout<<result;
+    return 0;
+
+
+    /**
+     * ! 3 2 1 0 Position (3)
+     * ! 8 4 2 1 Numbers System (13)
+     * ! 1 1 0 1 Binary Numbers
+    */
+
+}
