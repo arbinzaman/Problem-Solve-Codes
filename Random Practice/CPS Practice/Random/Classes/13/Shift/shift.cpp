66712bd46758f83719ab31fd04ea27e17841c4a4
arbinzaman <arbinzmn@gmail.com>
2024-09-14 23:04:47 +0600

    chore: Added Shit operator code cps Class13

---
+++ Random Practice/CPS Practice/Random/Classes/13/Shift/shift.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin >> a;
+    b = a << 3;/*left shit */
+    c= b >> 3;/*right shit*/
+    cout << b<< " " << c;
+
+}
