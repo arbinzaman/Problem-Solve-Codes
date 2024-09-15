6386aad6623de492e0c5dbb38fb859cd1edd2f86
arbinzaman <arbinzmn@gmail.com>
2024-09-15 14:06:30 +0600

    solved: ATCoder Basic problem

--- Random Practice/CPS Practice/AtCoder/Class/14/A - 2UP3DOWN/A2UP3DOWN.cpp
+++ Random Practice/CPS Practice/AtCoder/Class/14/A - 2UP3DOWN/A2UP3DOWN.cpp
@@ -9,17 +9,9 @@ int main (){
     else{
         cout << "No" << endl;
 }
+
     /**
      * !https://atcoder.jp/contests/abc326/tasks/abc326_a
-     *   int movingUp = y-x;
-         // int movingDown = x-y;
-
-         // if ((movingUp <= 2) && movingDown <=3){
-         //     cout<<"YES"<<endl;
-         // }
-         // else{
-         //     cout<<"NO"<<endl;
-         // }
-         // return 0;
+     
     */
 }
