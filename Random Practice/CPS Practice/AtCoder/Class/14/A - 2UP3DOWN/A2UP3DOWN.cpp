0872c15a17ec1342379e056641c29b92bbd4d466
arbinzaman <arbinzmn@gmail.com>
2024-09-15 14:00:58 +0600

    fixed: Accpted the problem in judged A2UP3DOWN

--- Random Practice/CPS Practice/AtCoder/Class/14/A - 2UP3DOWN/A2UP3DOWN.cpp
+++ Random Practice/CPS Practice/AtCoder/Class/14/A - 2UP3DOWN/A2UP3DOWN.cpp
@@ -3,19 +3,23 @@ using namespace std;
 int main (){
     int x ,y;
     cin >> x >> y;
-    int movingUp = y-x;
-    int movingDown = x-y;
-
-    if (movingUp <= 2 && movingDown <=3){
-        cout<<"YES"<<endl;
-    }
+    if((y>=x && y-x<=2) || (y<=x &&  x-y<=3)){
+        cout << "Yes" << endl;
+   }
     else{
-        cout<<"NO"<<endl;
-    }
-    return 0;
-
-
+        cout << "No" << endl;
+}
     /**
      * !https://atcoder.jp/contests/abc326/tasks/abc326_a
+     *   int movingUp = y-x;
+         // int movingDown = x-y;
+
+         // if ((movingUp <= 2) && movingDown <=3){
+         //     cout<<"YES"<<endl;
+         // }
+         // else{
+         //     cout<<"NO"<<endl;
+         // }
+         // return 0;
     */
 }
