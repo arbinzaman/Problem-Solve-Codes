0291e0fd3f64f5d969200401752e56efe76cdfbe
arbinzaman <arbinzmn@gmail.com>
2024-10-19 03:31:55 +0600

    chore: finised class 15 without solving B-Pentagon problem from ATcoder

---
+++ Random Practice/CPS Practice/AtCoder/Class/15/B - Pentagon/BPentagon.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+int n ;
+cin >> n;
+
+if  (n < 1000){
+    cout << n;
+}
+else if (n < 10000){
+    cout << (n/10)*10 << endl;
+}
+else if (n < 100000){
+    cout << (n/100)*100 << endl;
+}
+else if (n < 1000000){
+    cout << (n/1000)*1000 << endl;
+}
+else if (n < 10000000){
+    cout << (n/10000)*10000 << endl;
+}
+else if (n < 100000000){
+    cout << (n/100000)*100000 << endl;
+}
+else if (n < 1000000000){
+    cout << (n/1000000)*1000000 << endl;
+}
+
+
+    return 0;
+}
+
+/**
+ * TODO:  Problem link
+ * ! https://atcoder.jp/contests/abc304/tasks/abc304_b
+*/
