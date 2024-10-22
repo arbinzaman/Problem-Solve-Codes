8938bd80aae22e7a1eece6502efeefc0b0389265
arbinzaman <arbinzmn@gmail.com>
2024-10-23 01:49:12 +0600

    chore: Solved AtCoder Problem B-Pentagon problem

--- Random Practice/CPS Practice/AtCoder/Class/15/B - Pentagon/BPentagon.cpp
+++ Random Practice/CPS Practice/AtCoder/Class/15/B - Pentagon/BPentagon.cpp
@@ -1,13 +1,33 @@
 #include <bits/stdc++.h>
+#include <math.h>
 using namespace std;
-int main (){
-int n ;
-cin >> n;
-
+int main()
+{
+    char a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int dis1 = abs(b - a);
+    if (dis1 > 2){
+        dis1 = 5 - dis1;
+    }
+    int dis2 = abs(d-c);
+    if (dis2 > 2){
+        dis2 = 5 - dis2;
+    }
+    if(dis1 == dis2){
+        cout << "Yes"<<endl;
+    }
+    else{
+        cout << "No"<<endl;
+    }
     return 0;
 }
 
 /**
  * TODO:  Problem link
  * ! https://atcoder.jp/contests/abc304/tasks/abc304_b
-*/
+ * ? Done with help of ASCII code of characters
+ * ? The distance between two characters is the difference between their ASCII values
+ * ? If the distance is greater than 2, then the distance is 5 - distance
+ * ? If the distance between the two characters is same, then the answer is Yes
+ * ? Else the answer is No
+ */
