5e482600e57d3dfc93c442b2fb0c14baa6ac7739
arbinzaman <arbinzmn@gmail.com>
2024-10-25 00:45:33 +0600

    chore: basic loop with cps academey

---
+++ Random Practice/CPS Practice/Random/Classes/17/loop/randomPractice.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ for (int i=0; i<=10; i++)
+ {
+   cout << i<<" "<< "Argentina beats brasil" << endl;
+ }
+
+for (int i=5; i<=15; i+=2)
+{
+    cout << i <<" "<< "odd" << endl;
+}
+
+for (int i=5; i>=1; i--){
+    cout << i << " " << "countdown" << endl;
+}
+
+for (int i=-5; i<=5; i+=2)
+{
+   cout << i<< endl;
+}
+
+
+
+ return 0;
+}
+
+
+/**
+ * !Basic loop
+*/
