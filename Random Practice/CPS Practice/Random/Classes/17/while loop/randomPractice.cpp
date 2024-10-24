6ba1b50ee00bc999a6aea6b9d693122175aadd30
arbinzaman <arbinzmn@gmail.com>
2024-10-25 00:52:21 +0600

    chore: basic while loop with cps academy

---
+++ Random Practice/CPS Practice/Random/Classes/17/while loop/randomPractice.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+  int i = -5;
+  while (i <= 5)
+  {
+    cout << i << endl;
+    i+=2;
+  }
+
+  return 0;
+}
+
+/**
+ * !Basic loop
+ */
