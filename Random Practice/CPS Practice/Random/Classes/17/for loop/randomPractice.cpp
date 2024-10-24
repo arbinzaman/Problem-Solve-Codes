641ee745b5b5aeddc48ae6b3a8288c959fa8a8e1
arbinzaman <arbinzmn@gmail.com>
2024-10-25 01:10:16 +0600

    chore : break continue in loop

--- Random Practice/CPS Practice/Random/Classes/17/for loop/randomPractice.cpp
+++ Random Practice/CPS Practice/Random/Classes/17/for loop/randomPractice.cpp
@@ -2,31 +2,43 @@
 using namespace std;
 int main()
 {
- for (int i=0; i<=10; i++)
- {
-   cout << i<<" "<< "Argentina beats brasil" << endl;
- }
+  for (int i = 0; i <= 10; i++)
+  {
+    cout << i << " " << "Argentina beats brasil" << endl;
+  }
 
-for (int i=5; i<=15; i+=2)
-{
-    cout << i <<" "<< "odd" << endl;
-}
+  for (int i = 5; i <= 15; i += 2)
+  {
+    cout << i << " " << "odd" << endl;
+  }
 
-for (int i=5; i>=1; i--){
+  for (int i = 5; i >= 1; i--)
+  {
     cout << i << " " << "countdown" << endl;
+  }
+
+  for (int i = -5; i <= 5; i += 2)
+  {
+    cout << i << endl;
+  }
+
+  for (int i = 1; i <= 5; i++)
+  {
+    cout << i << " " << "break" << endl;
+    if (i == 3)
+      break;
+  }
+
+  for (int i = 1; i <= 5; i++)
+  {
+    if (i == 3)
+      continue;
+    cout << i << " " << "Continue" << endl;
+  }
+
+  return 0;
 }
-
-for (int i=-5; i<=5; i+=2)
-{
-   cout << i<< endl;
-}
-
-
-
- return 0;
-}
-
 
 /**
  * !Basic loop
-*/
+ */
