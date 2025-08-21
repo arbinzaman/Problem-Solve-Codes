3650eed2fc858a4ede32da927578c1b544128b35
Md Arbin Zaman <arbinzmn@gmail.com>
2025-08-21 16:09:37 +0600

    chore: basic object with parametter

--- Random Practice/Js/basicArrary.js
+++ Random Practice/Js/basicArrary.js
@@ -1,43 +1,9 @@
-<<<<<<< HEAD
 // console.log("Basic Array Operations");
 
 function basicArray() {
-    const numbers = [1, 2, 3, 4, 5];
-    const oddNumbers = [];
-    const evenNumbers = [];
-
-    console.log("Original Array:", numbers);
-
-    for (let num of numbers) {
-        if (num % 2 === 0) {
-            evenNumbers.push(num);
-        } else {
-            oddNumbers.push(num);
-        }
-    }
-
-    console.log("Even Numbers:", evenNumbers);
+	const numbers = [1, 2, 3, 4, 5];
+	const oddNumbers = numbers.filter((num) => num % 2 !== 0);
+	console.log(oddNumbers);
 }
 
 basicArray();
-
-function filterArray (){
-    const number = [1, 2, 3, 4, 5];
-    const evenNumber = number.filter(num => num % 2 === 0);
-    
-    console.log("Even Numbers:", evenNumber);
-}
-
-filterArray();
-=======
-// console.log("Basic Array Operations");
-
-
-function basicArray (){
-    const numbers = [1,2,3,4,5];
-    const oddNumbers = numbers.filter(num => num%2!==0)
-    console.log(oddNumbers);
-}
-
-basicArray();
->>>>>>> 5583466 (chore: fun)
---
+++ Random Practice/Js/basicObject.js
@@ -0,0 +1,5 @@
+const person = { name: "Arbin", age: 69 };
+function greet(obj) {
+	console.log(`Hello, my name is ${obj.name} and I am ${obj.age} years old`);
+}
+greet(person);
