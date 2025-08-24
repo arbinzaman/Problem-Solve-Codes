cbedd39e53c7db059555cbe9da966c57ceca6c3f
Md Arbin Zaman <arbinzmn@gmail.com>
2025-08-24 13:04:13 +0600

    chore: random

--- Random Practice/Js/basicLoop.js
+++ Random Practice/Js/basicLoop.js
@@ -1,7 +1,4 @@
-// FizzBuzz problem solution
-// Print numbers from 1 to 100
 for (let i = 1; i <= 100; i++) {
-	// console.log(i);
 	if (i % 3 === 0 && i % 5 === 0) {
 		console.log("FizzBuzz");
 	} else if (i % 3 === 0) {
