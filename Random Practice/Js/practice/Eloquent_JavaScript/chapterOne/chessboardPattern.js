1f3f8b3f12f8bd6a376d590c1d3a757c76545136
Md Arbin Zaman <arbinzmn@gmail.com>
2025-08-25 17:29:09 +0600

    chore: chessBorad

--- Random Practice/Js/practice/Eloquent_JavaScript/chapterOne/chessboardPattern.js
+++ Random Practice/Js/practice/Eloquent_JavaScript/chapterOne/chessboardPattern.js
@@ -1,6 +1,7 @@
-let board = ""
-let hash = "#"
 let size = 8
+let hash = "#"
+let board = ""
+
 for (let row = 0; row <= size; row++) {
 	for (let col = 0; col <= size; col++) {
 		if ((row + col) % 2 == 0) {
