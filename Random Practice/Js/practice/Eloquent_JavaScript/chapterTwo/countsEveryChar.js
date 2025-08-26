dbdce702f3983730f7b453255b8e6231fa6df49c
Md Arbin Zaman <arbinzmn@gmail.com>
2025-08-26 12:38:36 +0600

    chore: counts every char

---
+++ Random Practice/Js/practice/Eloquent_JavaScript/chapterTwo/countsEveryChar.js
@@ -0,0 +1,12 @@
+function countEveryChar(str) {
+	let count = {}
+	for (let char of str) {
+		if (count[char]) {
+			count[char]++
+		} else {
+			count[char] = 1
+		}
+	}
+	return count
+}
+console.log(countEveryChar("kirrrrreeeeeeeeeeeeeeebeeetttaaaa"))
