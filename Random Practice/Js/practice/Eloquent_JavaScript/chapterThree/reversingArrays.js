ffa4472c5caee489cb9b38d26d9c75ba7940d9e7
Md Arbin Zaman <arbinzmn@gmail.com>
2025-08-27 12:55:16 +0600

    chore: reverse arrary

--- Random Practice/Js/practice/Eloquent_JavaScript/chapterThree/reversingArrays.js
+++ Random Practice/Js/practice/Eloquent_JavaScript/chapterThree/reversingArrays.js
@@ -1,1 +1,9 @@
-
+let arr = [1, 2, 3, 4, 5]
+let newArr = []
+function reversedArray(arr) {
+	for (let i = arr.length - 1; i >= 0; --i) {
+		newArr.push(arr[i])
+	}
+	return newArr
+}
+console.log(reversedArray(arr))
