69414a7d8e7125157edc04e0ad15e5d319564898
Md Arbin Zaman <arbinzmn@gmail.com>
2025-09-28 17:09:02 +0600

    chore: solved two sum problem on leetcode

--- ProblemSolvedCodes/LeetCode/twoSum.js
+++ ProblemSolvedCodes/LeetCode/twoSum.js
@@ -1,2 +1,13 @@
-const num = [2, 7, 11, 15];
-const target = 9;
+const num = [3, 2, 3];
+const target = 6;
+function twoSum(num, target) {
+  for (let i = 0; i <= num.length; i++) {
+    for (let j = i + 1; j < num.length; j++) {
+      if (num[i] + num[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum(num, target));
